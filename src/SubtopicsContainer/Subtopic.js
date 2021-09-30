034cc35731d3cc5512e0bcf4f5af71c73d5c33e3
Ishaan Bose <ishaanbose2000@gmail.com>
2021-10-01 01:41:30 +0530

    web app fully responsive

--- src/SubtopicsContainer/Subtopic.js
+++ src/SubtopicsContainer/Subtopic.js
@@ -2,43 +2,16 @@ import React, { useState } from "react";
 import './Subtopic.css';
 
 const Subtopic = (props) => {
-    const [ isHover, setIsHover ] = useState(false);
-
-    const handleMouseOver = () => {
-        setIsHover(true);
-    };
-
-    const handleMouseOut = () => {
-        setIsHover(false);
-    };
-
     const handleClick = () => {
         props.showPage({
             showPage: true,
-            subject: props.type
+            subject: props.children
         });
     };
 
-    const getTextContentHoverClass = () => {
-        switch (props.type)
-        {
-            case "Functions" : return "text-content-hover-funcs";
-            case "Dictionaries" : return "text-content-hover-dicts";
-            default: return "text-content-hover-gen";
-        }
-    };
-
     return (
-        <div className="col-lg-4">
-            <div 
-                className={ `topic-${ props.type.toLowerCase() }` }
-                onMouseOver={ handleMouseOver }
-                onMouseOut={ handleMouseOut }
-                onClick={ handleClick } >
-                <div className={ isHover ? getTextContentHoverClass() : "text-content" }>
-                        { props.type }
-                </div>
-            </div>
+        <div id="subtopic-circle" className="subtopic-circle" onClick={ handleClick }>
+            <p className="text-content">{ props.children }</p>
         </div>
     );
 };
--- src/SubtopicsContainer/SubtopicsContainer.js
+++ src/SubtopicsContainer/SubtopicsContainer.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Subtopic from "./Subtopic";
 import fitText from "../scripts/fittext";
 import './Subtopic.css';
 
+const pages = ["Basics", "Strings", "Lists", "Tuples", "Dictionaries", "Functions", "Classes"];
+const n = pages.length;
+
 const SubtopicsContainer = (props) => {
     const [ containerInit, setContainerInit ] = useState(false);
+    const [ pageIndex, setPageIndex ] = useState(0);
 
     useEffect(() => {
         const timeoutID = setTimeout(() => {
             setContainerInit(true);
+            document.body.style.overflow = "auto";
         }, 100);
 
         return () => {
@@ -21,27 +26,44 @@ const SubtopicsContainer = (props) => {
         fitText(document.getElementById("subtopic-circle"));
     });
 
+    const nextPage = () => {
+        setPageIndex((((pageIndex + 1) % n) + n) % n);
+    };
+
+    const prevPage = () => {
+        setPageIndex((((pageIndex - 1) % n) + n) % n);
+    };
+
     return (
         <div className={containerInit ? "sub-topics-container-disp" : "sub-topics-container"}>
             <div className="row">
-                <div className="d-none d-lg-flex col-lg-4 col-md-3 justify-content-center">
-                    <button className="btn btn-primary">&lt;</button>
+                <div className="d-none d-lg-flex col-lg-4 col-md-3 justify-content-center align-items-center">
+                    <button className="nav-button" onClick={ prevPage }>
+                        <span className="material-icons large">arrow_back_ios</span>
+                    </button>
                 </div>
                 <div className="d-flex col-lg-4 col-md-12 col-sm-12 col-12 justify-content-center">
-                    <div id="subtopic-circle" className="subtopic-circle">
-                        <p className="text-content">Basics</p>
-                    </div>
+                    {/* <div id="subtopic-circle" className="subtopic-circle">
+                        <p className="text-content">{ pages[pageIndex] }</p>
+                    </div> */}
+                    <Subtopic showPage={ props.showPage }>{ pages[pageIndex] }</Subtopic>
                 </div>
-                <div className="d-none d-lg-flex col-lg-4 col-md-3 justify-content-center">
-                    <button className="btn btn-primary">&gt;</button>
+                <div className="d-none d-lg-flex col-lg-4 col-md-3 justify-content-center align-items-center">
+                    <button className="nav-button" onClick={ nextPage }>
+                        <span className="material-icons large">arrow_forward_ios</span>
+                    </button>
                 </div>
             </div>
             <div className="row d-sm-flex d-lg-none">
-                <div className="d-flex col-sm-6 col-6 justify-content-center">
-                    <button className="btn btn-primary">&lt;</button>
+                <div className="d-flex col-sm-6 col-6 justify-content-center flex-fill">
+                    <button className="nav-button-sm" onClick={ prevPage }>
+                        <span className="material-icons large">arrow_back_ios</span>
+                    </button>
                 </div>
-                <div className="d-flex col-sm-6 col-6 justify-content-center">
-                    <button className="btn btn-primary">&gt;</button>
+                <div className="d-flex col-sm-6 col-6 justify-content-center flex-fill">
+                    <button className="nav-button-sm" onClick={ nextPage }>
+                        <span className="material-icons large">arrow_forward_ios</span>
+                    </button>
                 </div>
             </div>
         </div>
